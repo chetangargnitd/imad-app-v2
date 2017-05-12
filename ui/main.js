14f744cee3891e03a968644c6e90085535ebef0c
Chetan Garg <chetangarg1102@gmail.com>
2017-05-12 22:10:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
-
+//change the text of main-text div
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+//move the image when we click on it
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
