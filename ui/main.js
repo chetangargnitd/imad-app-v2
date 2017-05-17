e3d209cfbbfa7d9231bcdc8285fb0fbf376f7894
Chetan Garg <chetangarg1102@gmail.com>
2017-05-17 10:39:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,24 @@
-console.log('Loaded!');
-//change the text of main-text div
-var element = document.getElementById('main-text');
+//Counter Code
+var button = document.getElementById("counter");
 
-element.innerHTML = 'New Value';
-
-//move the image when we click on it
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft +"px";
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+button.onclick = function () {
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the request and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readystate == XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status == 200) {
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+    };
+    //Make the request
+    request.open('GET', 'http://chetangargnitd.imad.hasura-app.io/counter' , true);
+    request.send(null);
 };
